432b89a69240938de8ee4825fe2f625d3bdad527
Chintan Shah <chintanshah@promactinfo.com>
2025-12-13 20:01:57 +0530

    feat: Configure GitHub Pages deployment and add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,24 @@ import starlight from '@astrojs/starlight';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [starlight({
-		title: 'GST Manual',
-		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/chintans/gst_manual_book' }],
-		sidebar: [
-			{
-				label: 'Table of Contents',
-				link: 'toc',
-			},
-			{
-				label: 'Book',
-				autogenerate: { directory: 'gst-manual' },
-			},
-		],
-	}), react()],
+    site: 'https://chintans.github.io',
+    base: '/gst_manual_book',
+    integrations: [starlight({
+        title: 'GST Manual',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/chintans/gst_manual_book' }],
+        sidebar: [
+            {
+                label: 'Table of Contents',
+                link: 'toc',
+            },
+            {
+                label: 'Book',
+                autogenerate: { directory: 'gst-manual' },
+            },
+        ],
+    }), react(), sitemap()],
 });
